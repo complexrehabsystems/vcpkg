1b02337792d755cf80933c5c7288cdd2c8499707
Daniel Packard <pack3754@gmail.com>
2018-04-03 11:18:07 -0500

    Add powershell script to configure CRS env variables for vcpkg installation

---
+++ ConfigureCrsVcpkgEnvironment.ps1
@@ -0,0 +1,4 @@
+﻿$VcpkgInstallRoot = (pwd).Path + "\installed\"
+
+[Environment]::SetEnvironmentVariable("VCPKG_ROOT_X86", $VcpkgInstallRoot + "x86-windows\", "Machine")
+[Environment]::SetEnvironmentVariable("VCPKG_ROOT_X64", $VcpkgInstallRoot + "x64-windows\", "Machine")
